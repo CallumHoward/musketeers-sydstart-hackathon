a1246d7a2048aa89bc8dca20aa03860585d5125f
Albert Kurniawan <kurniawan.albert19@gmail.com>
2015-10-29 12:46:13 +1100

    Added basic camera stuff that has not been tested

--- snaplancer/client/templates/homepage/homepage.js
+++ snaplancer/client/templates/homepage/homepage.js
@@ -1,6 +1,32 @@
 Template.homepage.helpers({
-  getFormattedPrice: function() {
-    return this.price.amount/100;
-  }
-
+  
 });
+
+Template.homepage.events({
+  'click a.link-hire': function(e) {
+    e.preventDefault();
+
+    if (Meteor.isCordova) {
+      // Open camera to take 13 seconds video
+      navigator.device.capture.captureVideo(captureSuccess, captureError, {
+        limit: 1, duration: 13
+      });
+    } else {
+      console.log('Not on mobile, no camera');
+    }
+  },
+});
+
+function captureSuccess(mediaFiles) {
+  var path = '';
+  for (let i = 0; i < mediaFiles.length; i++) {
+    path = mediaFiles[i].fullPath;
+    console.log(path);
+  }
+  Router.go('camera');
+}
+
+function captureError(error) {
+  console.log('Error capturing media!!!');
+  console.log(error);
+}
