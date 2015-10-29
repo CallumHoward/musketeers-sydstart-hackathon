2ab6a673ab0301b2201b3682babc3d106f3bdeed
Albert Kurniawan <kurniawan.albert19@gmail.com>
2015-10-29 19:39:18 +1100

    merge

---
+++ meteorApp/.meteor/local/.build780240.build/programs/web.browser/packages/tendo_freelancer-meteor.js
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// This is a generated file. You can view the original                  //
+// source in your browser if your browser supports source maps.         //
+// Source maps are supported by all recent versions of Chrome, Safari,  //
+// and Firefox, and by Internet Explorer 11.                            //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+var ECMAScript = Package.ecmascript.ECMAScript;
+var babelHelpers = Package['babel-runtime'].babelHelpers;
+var Symbol = Package['ecmascript-runtime'].Symbol;
+var Map = Package['ecmascript-runtime'].Map;
+var Set = Package['ecmascript-runtime'].Set;
+var Promise = Package.promise.Promise;
+
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package['tendo:freelancer-meteor'] = {};
+
+})();
---
+++ meteorApp/.meteor/local/isopacks/tendo_freelancer-meteor/os/freelancer-meteor.js
@@ -0,0 +1,4 @@
+Freelancer = Npm.require('freelancer-node')(
+  Meteor.settings.Freelancer.developerId,
+  Meteor.settings.Freelancer.developerKey
+);
